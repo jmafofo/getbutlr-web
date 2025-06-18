690c98eb17466866fa5502e7a92ec98cb1555cfb
Lawrence <claudiuslawrence@gmail.com>
2025-06-18 22:02:02 +0400

    add functions

--- supabase/functions/subscriptionManager/index.ts
+++ supabase/functions/subscriptionManager/index.ts
@@ -1,5 +1,5 @@
 import { serve } from "https://deno.land/std/http/server.ts";
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const supabase = createClient(
   Deno.env.get("SUPABASE_URL")!, Deno.env.get("SUPABASE_ANON_KEY")!
